fd5357328a63c214f006f544775885a7005346cb
cristian <cristiank09@hotmail.com>
2023-01-24 21:40:41 -0500

    Proyecto Barberia

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function Cambiar(imagen){
+    document.getElementById(imagen).src = 'image/logo.png';
+    document.getElementById(imagen).src = 'image/logo-blanco.png';
+}
+
+
+function elimina(elemento_elim){
+    elemento_elim.remove();
+    console.log(elimina);
+}
+
